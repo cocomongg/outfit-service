c935016e6cc19ff24c4476ae1f5b91b38a92020b
cocomongg <pjm4142@naver.com>
2025-04-04 00:46:12 +0900

    initial commit

---
+++ src/test/java/com/musinsa/snap/outfit/OutfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.snap.outfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
