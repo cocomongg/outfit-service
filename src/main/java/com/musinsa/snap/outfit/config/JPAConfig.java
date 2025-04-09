4490a8221233597576b92fdb2a643d662841a5c6
cocomongg <pjm4142@naver.com>
2025-04-09 15:46:43 +0900

    feat: brand 도메인 추가

---
+++ src/main/java/com/musinsa/snap/outfit/config/JPAConfig.java
@@ -0,0 +1,10 @@
+package com.musinsa.snap.outfit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JPAConfig {
+
+}
---
+++ src/main/java/com/musinsa/snap/outfit/domain/brand/model/Brand.java
@@ -0,0 +1,41 @@
+package com.musinsa.snap.outfit.domain.brand.model;
+
+import com.musinsa.snap.outfit.domain.common.BaseEntity;
+import com.musinsa.snap.outfit.domain.brand.dto.CreateBrandCommand;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "brands")
+public class Brand extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "brand_id")
+    public Long brandId;
+
+    @Column(name = "brand_name")
+    public String brandName;
+
+    @Column(name = "is_deleted")
+    public boolean deleted;
+
+    public Brand(CreateBrandCommand command) {
+        this.brandName = command.getBrandName();
+    }
+
+    public void update(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public void delete() {
+        this.deleted = true;
+    }
+}
